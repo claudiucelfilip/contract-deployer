53a9d34bf6371c9b855dbcee1a23c8d644f7b429
Claudiu Filip <claudiucelfilip@gmail.com>
2019-12-12 16:01:58 +0200

    refactore: set input url, rust path and key as inputs to exported function

--- index.js
+++ index.js
@@ -3,45 +3,53 @@ const exec = util.promisify(require("child_process").exec);
 const readFile = util.promisify(require("fs").readFile);
 const { Wavelet, JSBI } = require("wavelet-client");
 const path = require("path");
-(async () => {
-  const client = new Wavelet("http://localhost:9000");
-  const wallet = Wavelet.loadWalletFromPrivateKey(
-    "87a6813c3b4cf534b6ae82db9b1409fa7dbd5c13dba5858970b56084c4a930eb400056ee68a7cc2695222df05ea76875bc27ec6e61e8e62317c336157019c405"
+
+export default deploy = async (waveletUrl, privateKey, cargoPath) => {
+  const client = new Wavelet(waveletUrl);
+  const wallet = Wavelet.loadWalletFromPrivateKey(privateKey);
+
+  const buildResponse = await exec(
+    `cd ${path.resolve(
+      __dirname,
+      cargoPath
+    )} && cargo build --release --target wasm32-unknown-unknown`
   );
+  console.log("Contract build: \n", buildResponse.stdout, buildResponse.stderr);
 
-  try {
-    const res1 = await exec(
-      "cd .. && cargo build --release --target wasm32-unknown-unknown"
-    );
-    console.log("Contract build: \n", res1.stdout, res1.stderr);
+  console.log("........................................");
 
-    console.log("");
+  const wasmResponse = await exec(
+    `ls ${path.resolve(
+      __dirname,
+      cargoPath,
+      "target/wasm32-unknown-unknown/release/*.wasm"
+    )}`
+  );
+  console.log("Contract folder: \n", wasmResponse.stdout, wasmResponse.stderr);
 
-    const res2 = await exec(
-      "ls ../target/wasm32-unknown-unknown/release/*.wasm"
-    );
-    console.log("Contract folder: \n", res2.stdout, res2.stderr);
+  console.log("........................................");
 
-    console.log("");
+  const wasmPath = wasmResponse.stdout.trim();
+  const contractCode = await readFile(wasmPath);
+  console.log("Contract: \n", contractCode, toArrayBuffer(contractCode));
 
-    const res3 = await readFile(res2.stdout.trim());
-    console.log("Contract: \n", res3, toArrayBuffer(res3));
+  console.log("........................................");
 
-    console.log("");
+  const { id } = await client.deployContract(
+    wallet,
+    toArrayBuffer(res3),
+    JSBI.BigInt(100000)
+  );
+  console.log("Contract ID: \n", id);
 
-    const {id} = await client.deployContract(wallet, toArrayBuffer(res3), JSBI.BigInt(100000));
-    console.log("Contract ID: \n", id);
-  } catch (err) {
-    console.error(err);
-  }
-})();
-
+  return id;
+};
 
 function toArrayBuffer(buffer) {
   var ab = new ArrayBuffer(buffer.length);
   var view = new Uint8Array(ab);
   for (var i = 0; i < buffer.length; ++i) {
-      view[i] = buffer[i];
+    view[i] = buffer[i];
   }
   return ab;
 }
